08e3106766232bcf6c0fc84c24e569003ef1a71c
somekid <131805567+somekid-code@users.noreply.github.com>
2023-04-25 18:19:26 +0000

    first commit

---
+++ toat-aly normal code.py
@@ -0,0 +1,3 @@
+toat-aly normal code 
+when_downloaded_say_[give up human you've been infected}'
+play_image_https://www.thespruceeats.com/thmb/3C_YnXCs8TTjrhUz8dq1AkY25dI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/steamed-lobster-5097205-hero-06ab15ae53234b0ea162b60d54e57856.jpg_repeat_times_99999999999999999
